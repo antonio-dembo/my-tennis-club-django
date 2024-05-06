61e64c84919068dac45ea79a0aa1ffd66c583a35
antonio-dembo <beboantonio1@hotmail.com>
2024-05-06 19:58:00 -0300

    Add reader-friendly display for Admin tool

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-niz_%tocrk++t7x4n19q571$09df1%4kgla$abgrxr9xrn-7xy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
